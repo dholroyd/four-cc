a9fc13005c483c0aec5ddd8b34d8bd56a31fe41e
David Holroyd <dave@badgers-in-foil.co.uk>
2024-03-16 09:30:58 +0000

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -90,26 +90,24 @@ use core::result::Result;
 /// A _four-character-code_ value.
 ///
 /// See the [module level documentation](index.html).
-#[derive(Clone,Copy,PartialEq,Eq,Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "zerocopy", derive(zerocopy::FromBytes, zerocopy::AsBytes))]
 #[repr(C, packed)]
-pub struct FourCC (
-    pub [u8; 4]
-);
+pub struct FourCC(pub [u8; 4]);
 impl FourCC {
     const fn from_u32(self: Self) -> u32 {
-        ((self.0[0] as u32) << 24 & 0xff000000) |
-        ((self.0[1] as u32) << 16 & 0x00ff0000) |
-        ((self.0[2] as u32) << 8  & 0x0000ff00) |
-        ((self.0[3] as u32)       & 0x000000ff)
+        ((self.0[0] as u32) << 24 & 0xff000000)
+            | ((self.0[1] as u32) << 16 & 0x00ff0000)
+            | ((self.0[2] as u32) << 8 & 0x0000ff00)
+            | ((self.0[3] as u32) & 0x000000ff)
     }
 }
-impl<'a> From<&'a[u8; 4]> for FourCC {
+impl<'a> From<&'a [u8; 4]> for FourCC {
     fn from(buf: &[u8; 4]) -> FourCC {
         FourCC([buf[0], buf[1], buf[2], buf[3]])
     }
 }
-impl<'a> From<&'a[u8]> for FourCC {
+impl<'a> From<&'a [u8]> for FourCC {
     fn from(buf: &[u8]) -> FourCC {
         FourCC([buf[0], buf[1], buf[2], buf[3]])
     }
@@ -119,8 +117,8 @@ impl From<u32> for FourCC {
         FourCC([
             (val >> 24 & 0xff) as u8,
             (val >> 16 & 0xff) as u8,
-            (val >> 8  & 0xff) as u8,
-            (val       & 0xff) as u8
+            (val >> 8 & 0xff) as u8,
+            (val & 0xff) as u8,
         ])
     }
 }
@@ -155,12 +153,14 @@ impl fmt::Display for FourCC {
                 // If we return fmt::Error, then for example format!() will panic, so we choose
                 // an alternative representation instead
                 let b = &self.0;
-                f.write_fmt(format_args!("{}{}{}{}",
-                                         core::ascii::escape_default(b[0]),
-                                         core::ascii::escape_default(b[1]),
-                                         core::ascii::escape_default(b[2]),
-                                         core::ascii::escape_default(b[3])))
-            },
+                f.write_fmt(format_args!(
+                    "{}{}{}{}",
+                    core::ascii::escape_default(b[0]),
+                    core::ascii::escape_default(b[1]),
+                    core::ascii::escape_default(b[2]),
+                    core::ascii::escape_default(b[3])
+                ))
+            }
         }
     }
 }
@@ -169,12 +169,14 @@ impl fmt::Debug for FourCC {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         let b = self.0;
         f.debug_tuple("FourCC")
-          .field(&format_args!("{}{}{}{}",
-                               core::ascii::escape_default(b[0]),
-                               core::ascii::escape_default(b[1]),
-                               core::ascii::escape_default(b[2]),
-                               core::ascii::escape_default(b[3])))
-          .finish()
+            .field(&format_args!(
+                "{}{}{}{}",
+                core::ascii::escape_default(b[0]),
+                core::ascii::escape_default(b[1]),
+                core::ascii::escape_default(b[2]),
+                core::ascii::escape_default(b[3])
+            ))
+            .finish()
     }
 }
 
